d0565e57c77d87dee08b93daa934717cde9442cc
Obiora Igboanusi <cwertlinks@gmail.com>
2023-11-03 03:40:31 +0100

    Configure default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: '#8311F9',
+                    600: '#8311F9',
+                    200: '#CF94FF',
+                },
+                gray: {
+                    DEFAULT: '#181818',
+                    900: '#181818',
+                    600: '#6A6A6A',
+                    400: '#B2B2B2',
+                    300: '#D6D6D6',
+                    200: '#E7E7E7',
+                    100: '#C7C4BC', // abstract
+                },
+            },
+        },
+    },
+    plugins: [],
+};
