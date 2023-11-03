d4875b54bf0adc5cb338a876bdbe67a5733f192a
Obiora Igboanusi <cwertlinks@gmail.com>
2023-11-03 05:10:22 +0100

    Configure path alias

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,10 @@ export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './src/'),
-            assets: `${path.resolve(__dirname, './src/assets/')}`,
-            pages: `${path.resolve(__dirname, './src/pages/')}`,
-            components: `${path.resolve(__dirname, './src/components/')}`,
+            '@': `${path.resolve(__dirname, './src/')}`,
+            '@assets': `${path.resolve(__dirname, './src/assets/')}`,
+            '@pages': `${path.resolve(__dirname, './src/pages/')}`,
+            '@components': `${path.resolve(__dirname, './src/components/')}`,
         },
     },
 });
