57da1253f60f154f4db9807252961cf09a73771c
Obiora Igboanusi <cwertlinks@gmail.com>
2023-11-04 12:45:21 +0100

    Implement scroll to top

--- src/components/Landing/Posts.tsx
+++ src/components/Landing/Posts.tsx
@@ -1,3 +1,4 @@
+import ArrowUp from '@assets/ArrowUp';
 import Category from '@components/Category';
 import PostCard from '@components/PostCard';
 import Tabs, { ITab } from '@components/Tabs';
@@ -27,9 +28,15 @@ const tabs: ITab[] = [
 ];
 
 function Posts() {
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth', // Optional: smooth scrolling animation
+        });
+    };
     return (
-        <section className="bg-white py-[60px]">
-            <div className="container">
+        <section className="bg-white py-[60px] mb-[250px]">
+            <div className="container relative">
                 <div className="grid grid-cols-3 gap-4">
                     <div className="col-span-3 lg:col-span-2">
                         <Tabs name="tab" tabs={tabs} />
@@ -54,6 +61,14 @@ function Posts() {
                             ))}
                         </ul>
                     </div>
+                </div>
+                <div className="absolute bottom-[2rem] right-[1rem]">
+                    <button
+                        onClick={scrollToTop}
+                        className="h-[60px] w-[60px] rounded-full bg-primary-200 flex justify-center items-center hover:opacity-75 hover:-translate-y-[2px]"
+                    >
+                        <ArrowUp />
+                    </button>
                 </div>
             </div>
         </section>
