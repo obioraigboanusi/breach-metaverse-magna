e39033c9833c7f70a86573c502b52626fe51a22c
Obiora Igboanusi <cwertlinks@gmail.com>
2023-11-04 14:10:33 +0100

    Implement registration from

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,12 @@
 import Logo from '@assets/Logo';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Navbar() {
+    const { pathname } = useLocation();
+    const isAuthPath = pathname.includes('/auth/');
+
     return (
-        <nav className="bg-[#FCFAFF]">
+        <nav className={isAuthPath ? 'bg-white' : 'bg-[#FCFAFF]'}>
             <div className="container  py-[36px]">
                 <div className="flex justify-between items-center">
                     <div>
@@ -11,14 +14,16 @@ function Navbar() {
                             <Logo />
                         </Link>
                     </div>
-                    <div className="flex gap-[32px] items-center">
-                        <Link to="/login" className="btn btn-outline">
-                            Log in
-                        </Link>
-                        <Link to="register" className="btn btn-primary">
-                            Join Breach
-                        </Link>
-                    </div>
+                    {!isAuthPath && (
+                        <div className="flex gap-[32px] items-center">
+                            <Link to="/auth/login" className="btn btn-outline">
+                                Log in
+                            </Link>
+                            <Link to="/auth/register" className="btn btn-primary">
+                                Join Breach
+                            </Link>
+                        </div>
+                    )}
                 </div>
             </div>
         </nav>
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import Landing from './pages/Landing';
+import Register from '@pages/Register';
+import Login from '@pages/Login';
+
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Landing />,
+    },
+    {
+        path: '/auth/login',
+        element: <Login />,
+    },
+    {
+        path: '/auth/register',
+        element: <Register />,
     },
 ]);
 
