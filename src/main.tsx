cc8dbfc84d166aba524b92f220b5afb74c025953
Obiora Igboanusi <cwertlinks@gmail.com>
2023-11-05 18:11:06 +0100

    Configure react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,57 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
-import Landing from './pages/Landing';
-import Register from '@pages/Register';
-import Login from '@pages/Login';
-import Welcome from '@pages/Welcome';
-import UserInterests from '@pages/UserInterests';
-import Dashboard from '@pages/Home';
-import UserLayout from '@components/UserLayout';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RouterProvider } from 'react-router-dom';
+import router from '@routes';
 
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Landing />,
-    },
-    {
-        path: '/auth/login',
-        element: <Login />,
-    },
-    {
-        path: '/auth/register',
-        element: <Register />,
-    },
-    {
-        path: '/user/welcome',
-        element: <Welcome />,
-    },
-    {
-        path: '/user/interests',
-        element: <UserInterests />,
-    },
-    {
-        element: <UserLayout />,
-        children: [
-            {
-                path: '/user/home',
-                element: <Dashboard />,
-            },
-            {
-                path: '/user/dashboard',
-                element: <div>Dashboard</div>,
-            },
-            {
-                path: '/user/publications',
-                element: <div>Publications</div>,
-            },
-        ],
-    },
-]);
+import './index.css';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+        </QueryClientProvider>
     </React.StrictMode>
 );
