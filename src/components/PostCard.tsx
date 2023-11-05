035a5edaf7cb55503be7c6dcec8c9c34b6a70fa7
Obiora Igboanusi <cwertlinks@gmail.com>
2023-11-05 18:15:27 +0100

    Consume api for posts

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,21 +1,22 @@
-import postImage from '@assets/post.png';
+import moment from 'moment';
 
-function PostCard() {
+function PostCard({ post }) {
+    const { title, content, author, createdAt, series, imageUrl } = post || {};
+    const date = moment(new Date(createdAt)).format('DD MMM YYYY');
+
     return (
         <div className="post-card grid gap-[28px]">
             <div className="h-[186px] rounded-2xl border border-gray-300">
-                <img src={postImage} alt="" className="w-full h-full max-h-[184px] object-cover object-center rounded-2xl" />
+                <img src={imageUrl} alt="" className="w-full h-full max-h-[184px] object-cover object-center rounded-2xl" />
             </div>
             <div className="">
-                <p className="uppercase mb-4 text-sm text-gray-600">Work in Progress</p>
-                <h3 className="text-2xl font-semibold mb-3">On migration and maintaining friendships</h3>
-                <p className="mb-3 text-gray-600">
-                    I went to boarding school and left pretty early, so I had some experience with losing friends to relocation long before the
-                </p>
+                <p className="uppercase mb-4 text-sm text-gray-600">{series.name}</p>
+                <h3 className="text-2xl font-semibold mb-3">{title}</h3>
+                <p className="mb-3 text-gray-600">{content}</p>
                 <div className="flex gap-3 items-center text-xs">
-                    <span>Lota Anidi</span>
-                    <span className="block w-[4px] h-[4px] bg-gray rounded-full" />
-                    <span>12 Dec 2022</span>
+                    <span>{author.name}</span>
+                    <div className="w-[4px] h-[4px] bg-gray rounded-full" />
+                    <span>{date?.toUpperCase()}</span>
                 </div>
             </div>
         </div>
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,5 +1,6 @@
 import Tabs, { ITab } from '@components/Tabs';
 import PostCard from './PostCard';
+import { useGetPosts } from '@hooks/useBlogServices';
 
 const tabs: ITab[] = [
     { label: 'Featured', value: 'featured' },
@@ -8,13 +9,16 @@ const tabs: ITab[] = [
 ];
 
 function PostList() {
+    const { data: posts } = useGetPosts();
+    console.log({ posts });
+
     return (
         <div>
             <Tabs name="tab" tabs={tabs} />
             <ul className="grid grid-cols-1 gap-[64px] mt-[40px]">
-                {Array.from({ length: 10 }).map(() => (
-                    <li>
-                        <PostCard />
+                {posts?.map((post) => (
+                    <li key={'post-item-' + post.id}>
+                        <PostCard post={post} />
                     </li>
                 ))}
             </ul>
